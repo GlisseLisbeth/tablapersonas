d88d116675e8e78f90fe214ee3ce62a12877c694
glisselisbethjm <glisse1986.jm@outlook.com>
2019-10-19 01:39:24 -0500

    Updates

---
+++ precache-manifest.fc9767216690b7feffd626cc99ff8883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2228e552976b765e6cd8b1718b78732b",
+    "url": "/tablapersonas/index.html"
+  },
+  {
+    "revision": "0241096b51b0d8c7f514",
+    "url": "/tablapersonas/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "3b487e5b6a99d93da2e8",
+    "url": "/tablapersonas/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "0241096b51b0d8c7f514",
+    "url": "/tablapersonas/static/js/2.63f8eff6.chunk.js"
+  },
+  {
+    "revision": "3b487e5b6a99d93da2e8",
+    "url": "/tablapersonas/static/js/main.d0238211.chunk.js"
+  },
+  {
+    "revision": "8c736063af6f4cdfa14d",
+    "url": "/tablapersonas/static/js/runtime-main.637ba046.js"
+  }
+]);
